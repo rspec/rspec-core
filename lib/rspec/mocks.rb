c74e5e63a056b501261f2d687b9831374f9e003a
David Chelimsky <dchelimsky@gmail.com>
2010-05-20 06:59:32 -0500

    lots of words

--- lib/rspec/mocks.rb
+++ lib/rspec/mocks.rb
@@ -3,108 +3,73 @@ require 'rspec/mocks/extensions/object'
 require 'rspec/mocks/version'
 
 module RSpec
-  # == Mocks and Stubs
+  # == Test Doubles
   #
-  # RSpec will create Mock Objects and Stubs for you at runtime, or attach stub/mock behaviour
-  # to any of your real objects (Partial Mock/Stub). Because the underlying implementation
-  # for mocks and stubs is the same, you can intermingle mock and stub
-  # behaviour in either dynamically generated mocks or your pre-existing classes.
-  # There is a semantic difference in how they are created, however,
-  # which can help clarify the role it is playing within a given spec.
+  # A Test Double is an object that stands in for a real object in a test.
+  # RSpec creates test doubles that support method stubs and message
+  # expectations.
   #
-  # == Mock Objects
-  # 
-  # Mocks are objects that allow you to set and verify expectations that they will
-  # receive specific messages during run time. They are very useful for specifying how the subject of
-  # the spec interacts with its collaborators. This approach is widely known as "interaction
-  # testing".
-  # 
-  # Mocks are also very powerful as a design tool. As you are
-  # driving the implementation of a given class, Mocks provide an anonymous
-  # collaborator that can change in behaviour as quickly as you can write an expectation in your
-  # spec. This flexibility allows you to design the interface of a collaborator that often
-  # does not yet exist. As the shape of the class being specified becomes more clear, so do the
-  # requirements for its collaborators - often leading to the discovery of new types that are
-  # needed in your system.
-  # 
-  # Read Endo-Testing[http://www.mockobjects.com/files/endotesting.pdf] for a much
-  # more in depth description of this process.
-  # 
-  # == Stubs
-  # 
-  # Stubs are objects that allow you to set "stub" responses to
-  # messages. As Martin Fowler points out on his site,
-  # mocks_arent_stubs[http://www.martinfowler.com/articles/mocksArentStubs.html].
-  # Paraphrasing Fowler's paraphrasing
-  # of Gerard Meszaros: Stubs provide canned responses to messages they might receive in a test, while
-  # mocks allow you to specify and, subsquently, verify that certain messages should be received during
-  # the execution of a test.
-  # 
-  # == Partial Mocks/Stubs
-  # 
-  # RSpec also supports partial mocking/stubbing, allowing you to add stub/mock behaviour
-  # to instances of your existing classes. This is generally
-  # something to be avoided, because changes to the class can have ripple effects on
-  # seemingly unrelated specs. When specs fail due to these ripple effects, the fact
-  # that some methods are being mocked can make it difficult to understand why a
-  # failure is occurring.
-  # 
-  # That said, partials do allow you to expect and
-  # verify interactions with class methods such as +#find+ and +#create+
-  # on Ruby on Rails model classes.
-  # 
-  # == Further Reading
-  # 
-  # There are many different viewpoints about the meaning of mocks and stubs. If you are interested
-  # in learning more, here is some recommended reading:
-  # 
-  # * Mock Objects: http://www.mockobjects.com/
-  # * Endo-Testing: http://www.mockobjects.com/files/endotesting.pdf
-  # * Mock Roles, Not Objects: http://www.mockobjects.com/files/mockrolesnotobjects.pdf
-  # * Test Double Patterns: http://xunitpatterns.com/Test%20Double%20Patterns.html
-  # * Mocks aren't stubs: http://www.martinfowler.com/articles/mocksArentStubs.html
+  #   book = double("book")
   #
-  # == Creating a Mock
+  # == Method Stubs
   #
-  # You can create a mock in any specification (or setup) using:
+  # A method stub is an implementation that returns a pre-determined value.
   #
-  #   double(name, options={})
+  #   book = double("book")
+  #   double.stub(:title) { "The RSpec Book" }
+  #   double.title => "The RSpec Book"
   #
-  # The optional +options+ argument is a +Hash+. Currently the only supported
-  # option is +:null_object+. Setting this to true instructs the mock to ignore
-  # any messages it hasn’t been told to expect – and quietly return itself. For example:
+  # When we declare a stub, we way we "stubbing" a method.
   #
-  #   double("person", :null_object => true)
+  # == Message Expectations
   #
-  # == Creating a Stub
+  # A message expectation is an expectation that the test double will receive a
+  # message some time before the example ends. If the message is received, the
+  # expectation is satisfied. If not, the example fails.
   #
-  # You can create a stub in any specification (or setup) using:
-  #
-  #   stub(name, stub_methods_and_values_hash)
-  #
-  # For example, if you wanted to create an object that always returns
-  # "More?!?!?!" to "please_sir_may_i_have_some_more" you would do this:
-  #
-  #   stub("Mr Sykes", :please_sir_may_i_have_some_more => "More?!?!?!")
-  #
-  # == Creating a Partial Mock
-  #
-  # You don't really "create" a partial mock, you simply add method stubs and/or
-  # mock expectations to existing classes and objects:
-  #
-  #   Factory.should_receive(:find).with(id).and_return(value)
-  #   obj.stub(:to_i).and_return(3)
-  #   etc ...
-  #
-  # == Expecting Messages
-  #
-  #   my_mock.should_receive(:sym)
-  #   my_mock.should_not_receive(:sym)
+  #   registrar = double("registrar")
+  #   validator.should_receive(:validate).with("02134")
+  #   zipcode = Zipcode.new("02134", validator)
+  #   zipcode.valid?
   #   
+  # When we declare a message expectation, we way we "mocking" a method.
+  #
+  # == Mock Objects and Test Stubs
+  #
+  # The names Mock Object and Test Stub suggest specialized Test Doubles.  i.e.
+  # Test Stub evokes Test Double that only supports method stubs, and a Mock
+  # Object evokes a Test Double that only supports message expectations, or
+  # sometimes supports message expectations in addition to method stubs.
+  #
+  # There is a lot of overlapping nomenclature here, and there are many
+  # variations of these patterns (fakes, spies, etc). Keep in mind that most of
+  # the time we're talking about method-level concepts that are variations of
+  # method stubs and message expectations, and we're applying to them to _one_
+  # generic kind of object: a Test Double.
+  #
+  # == Test-Specific Extension
+  #
+  # a.k.a. Partial Stub/Mock, a Test-Specific Extension is an extension of a
+  # real object in a system that is instrumented with test-double like
+  # behaviour in the context of a test. This technique is very common in Ruby
+  # because we often see class objects acting as global namespaces for methods.
+  # For example, in Rails:
+  #
+  #   person = double("person")
+  #   Person.stub(:find) { person }
+  #
+  # In this case we're instrumenting Person to return the person object we've
+  # defined whenever it receives the +find+ message. We can do this with any
+  # object in a system because RSpec adds the +stub+ and +should_receive+
+  # methods to every object. When we use either, RSpec replaces the method
+  # we're stubbing or mocking with it's own test-double-like method. At the
+  # end of the example, RSpec verifies any message expectations, and then
+  # restores the original methods.
+  # 
   # == Expecting Arguments
   #
-  #   my_mock.should_receive(:sym).with(*args)
-  #   my_mock.should_not_receive(:sym).with(*args)
+  #   double.should_receive(:msg).with(*args)
+  #   double.should_not_receive(:msg).with(*args)
   #
   # == Argument Matchers
   #
@@ -119,61 +84,64 @@ module RSpec
   # In addition, RSpec::Mocks adds some keyword Symbols that you can use to
   # specify certain kinds of arguments:
   #
-  #   my_mock.should_receive(:sym).with(no_args())
-  #   my_mock.should_receive(:sym).with(any_args())
-  #   my_mock.should_receive(:sym).with(1, kind_of(Numeric), "b") #2nd argument can any kind of Numeric
-  #   my_mock.should_receive(:sym).with(1, boolean(), "b") #2nd argument can true or false
-  #   my_mock.should_receive(:sym).with(1, /abc/, "b") #2nd argument can be any String matching the submitted Regexp
-  #   my_mock.should_receive(:sym).with(1, anything(), "b") #2nd argument can be anything at all
-  #   my_mock.should_receive(:sym).with(1, ducktype(:abs, :div), "b")
+  #   double.should_receive(:msg).with(no_args())
+  #   double.should_receive(:msg).with(any_args())
+  #   double.should_receive(:msg).with(1, kind_of(Numeric), "b") #2nd argument can any kind of Numeric
+  #   double.should_receive(:msg).with(1, boolean(), "b") #2nd argument can true or false
+  #   double.should_receive(:msg).with(1, /abc/, "b") #2nd argument can be any String matching the submitted Regexp
+  #   double.should_receive(:msg).with(1, anything(), "b") #2nd argument can be anything at all
+  #   double.should_receive(:msg).with(1, ducktype(:abs, :div), "b")
   #                            #2nd argument can be object that responds to #abs and #div
   #                                                                       
   # == Receive Counts
   #
-  #   my_mock.should_receive(:sym).once
-  #   my_mock.should_receive(:sym).twice
-  #   my_mock.should_receive(:sym).exactly(n).times
-  #   my_mock.should_receive(:sym).at_least(:once)
-  #   my_mock.should_receive(:sym).at_least(:twice)
-  #   my_mock.should_receive(:sym).at_least(n).times
-  #   my_mock.should_receive(:sym).at_most(:once)
-  #   my_mock.should_receive(:sym).at_most(:twice)
-  #   my_mock.should_receive(:sym).at_most(n).times
-  #   my_mock.should_receive(:sym).any_number_of_times
+  #   double.should_receive(:msg).once
+  #   double.should_receive(:msg).twice
+  #   double.should_receive(:msg).exactly(n).times
+  #   double.should_receive(:msg).at_least(:once)
+  #   double.should_receive(:msg).at_least(:twice)
+  #   double.should_receive(:msg).at_least(n).times
+  #   double.should_receive(:msg).at_most(:once)
+  #   double.should_receive(:msg).at_most(:twice)
+  #   double.should_receive(:msg).at_most(n).times
+  #   double.should_receive(:msg).any_number_of_times
   #
   # == Ordering
   #
-  #   my_mock.should_receive(:sym).ordered
-  #   my_mock.should_receive(:other_sym).ordered
+  #   double.should_receive(:msg).ordered
+  #   double.should_receive(:other_msg).ordered
   #     #This will fail if the messages are received out of order
   #
   # == Setting Reponses
   #
-  # Whether you are setting a mock expectation or a simple stub, you can tell the
-  # object precisely how to respond:
+  # Whether you are setting a message expectation or a method stub, you can
+  # tell the object precisely how to respond. The most generic way is to pass
+  # a block to +stub+ or +should_receive+:
   #
-  #   my_mock.should_receive(:sym).and_return(value)
-  #   my_mock.should_receive(:sym).exactly(3).times.and_return(value1, value2, value3)
+  #   double.should_receive(:msg) { value }
+  #
+  # When the double receives the +msg+ message, it evaluates the block and returns
+  # the result.
+  #
+  #   double.should_receive(:msg).and_return(value)
+  #   double.should_receive(:msg).exactly(3).times.and_return(value1, value2, value3)
   #     # returns value1 the first time, value2 the second, etc
-  #   my_mock.should_receive(:sym).and_return { ... } #returns value returned by the block
-  #   my_mock.should_receive(:sym).and_raise(error)
+  #   double.should_receive(:msg).and_raise(error)
   #     #error can be an instantiated object or a class
   #     #if it is a class, it must be instantiable with no args
-  #   my_mock.should_receive(:sym).and_throw(:sym)
-  #   my_mock.should_receive(:sym).and_yield(values,to,yield)
-  #   my_mock.should_receive(:sym).and_yield(values,to,yield).and_yield(some,other,values,this,time)
+  #   double.should_receive(:msg).and_throw(:msg)
+  #   double.should_receive(:msg).and_yield(values,to,yield)
+  #   double.should_receive(:msg).and_yield(values,to,yield).and_yield(some,other,values,this,time)
   #     # for methods that yield to a block multiple times
   #
-  # Any of these responses can be applied to a stub as well, but stubs do
-  # not support any qualifiers about the message received (i.e. you can't specify arguments
-  # or receive counts):
+  # Any of these responses can be applied to a stub as well
   #
-  #   my_mock.stub(:sym).and_return(value)
-  #   my_mock.stub(:sym).and_return(value1, value2, value3)
-  #   my_mock.stub(:sym).and_raise(error)
-  #   my_mock.stub(:sym).and_throw(:sym)
-  #   my_mock.stub(:sym).and_yield(values,to,yield)
-  #   my_mock.stub(:sym).and_yield(values,to,yield).and_yield(some,other,values,this,time)
+  #   double.stub(:msg).and_return(value)
+  #   double.stub(:msg).and_return(value1, value2, value3)
+  #   double.stub(:msg).and_raise(error)
+  #   double.stub(:msg).and_throw(:msg)
+  #   double.stub(:msg).and_yield(values,to,yield)
+  #   double.stub(:msg).and_yield(values,to,yield).and_yield(some,other,values,this,time)
   #
   # == Arbitrary Handling
   #
@@ -182,21 +150,28 @@ module RSpec
   # to come with an Array argument that has a specific length, but you don't care
   # what is in it. You could do this:
   #
-  #   my_mock.should_receive(:sym) do |arg|
+  #   double.should_receive(:msg) do |arg|
   #     arg.should be_an_istance_of(Array)
   #     arg.length.should == 7
   #   end
-  #
-  # Note that this would fail if the number of arguments received was different from
-  # the number of block arguments (in this case 1).
   #
   # == Combining Expectation Details
   #
   # Combining the message name with specific arguments, receive counts and responses
   # you can get quite a bit of detail in your expectations:
   #
-  #   my_mock.should_receive(:<<).with("illegal value").once.and_raise(ArgumentError)
+  #   double.should_receive(:<<).with("illegal value").once.and_raise(ArgumentError)
+  #
+  # == Further Reading
+  # 
+  # There are many different viewpoints about the meaning of mocks and stubs. If you are interested
+  # in learning more, here is some recommended reading:
+  # 
+  # * Mock Objects: http://www.mockobjects.com/
+  # * Endo-Testing: http://www.mockobjects.com/files/endotesting.pdf
+  # * Mock Roles, Not Objects: http://www.mockobjects.com/files/mockrolesnotobjects.pdf
+  # * Test Double Patterns: http://xunitpatterns.com/Test%20Double%20Patterns.html
+  # * Mocks aren't stubs: http://www.martinfowler.com/articles/mocksArentStubs.html
   module Mocks
   end
-  
 end
