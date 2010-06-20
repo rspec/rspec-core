a918526e09acc4950a29ad10ff00fa208a7271f7
David Chelimsky <dchelimsky@gmail.com>
2010-06-20 11:32:14 -0500

    define subscriptions before other files are required

--- lib/rspec/core.rb
+++ lib/rspec/core.rb
@@ -1,3 +1,57 @@
+module RSpec
+  class << self
+    def subscriptions
+      @subscriptions ||= Hash.new {|h,k| h[k] = []}
+    end
+
+    # Supported events:
+    #   example_group_started
+    #   example_started
+    #   example_initialized
+    #   example_executed
+    #   example_finished
+    def subscribe(event, &callback)
+      subscriptions[event] << callback
+    end
+
+    def publish(event, *args)
+      subscriptions[event].each {|callback| callback.call(*args)}
+    end
+    def world
+      @world ||= RSpec::Core::World.new
+    end
+
+    def configuration
+      @configuration ||= RSpec::Core::Configuration.new
+    end
+
+    def configure
+      yield configuration if block_given?
+    end
+  end
+
+  module Core
+    def self.install_directory
+      @install_directory ||= File.expand_path(File.dirname(__FILE__))
+    end
+
+    def self.configuration
+      RSpec.deprecate('RSpec::Core.configuration', 'RSpec.configuration', '2.0.0')
+      RSpec.configuration
+    end
+
+    def self.configure
+      RSpec.deprecate('RSpec::Core.configure', 'RSpec.configure', '2.0.0')
+      yield RSpec.configuration if block_given?
+    end
+
+    def self.world
+      RSpec.deprecate('RSpec::Core.world', 'RSpec.world', '2.0.0')
+      RSpec.world
+    end
+  end
+end
+
 require 'rspec/core/kernel_extensions'
 require 'rspec/core/object_extensions'
 require 'rspec/core/load_path'
@@ -24,62 +78,6 @@ require 'rspec/core/shared_example_group'
 require 'rspec/core/example_group'
 require 'rspec/core/version'
 require 'rspec/core/errors'
-
-module RSpec
-  module Core
-
-    def self.install_directory
-      @install_directory ||= File.expand_path(File.dirname(__FILE__))
-    end
-
-    def self.configuration
-      RSpec.deprecate('RSpec::Core.configuration', 'RSpec.configuration', '2.0.0')
-      RSpec.configuration
-    end
-
-    def self.configure
-      RSpec.deprecate('RSpec::Core.configure', 'RSpec.configure', '2.0.0')
-      yield RSpec.configuration if block_given?
-    end
-
-    def self.world
-      RSpec.deprecate('RSpec::Core.world', 'RSpec.world', '2.0.0')
-      RSpec.world
-    end
-
-  end
-
-  def self.world
-    @world ||= RSpec::Core::World.new
-  end
-
-  def self.configuration
-    @configuration ||= RSpec::Core::Configuration.new
-  end
-
-  def self.configure
-    yield configuration if block_given?
-  end
-
-  def self.subscriptions
-    @subscriptions ||= Hash.new {|h,k| h[k] = []}
-  end
-
-  # Supported events:
-  #   example_group_started
-  #   example_started
-  #   example_initialized
-  #   example_executed
-  #   example_finished
-  def self.subscribe(event, &callback)
-    subscriptions[event] << callback
-  end
-
-  def self.publish(event, *args)
-    subscriptions[event].each {|callback| callback.call(*args)}
-  end
-end
-
 require 'rspec/core/backward_compatibility'
 
 # TODO - make this configurable with default 'on'
