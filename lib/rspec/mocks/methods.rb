8bb5142642e75be0b40c480294208de9b8c0c2e4
David Chelimsky <dchelimsky@gmail.com>
2010-10-02 13:14:31 -0500

    Revert "Fixed YAML serialization of a stubbed object."
    This reverts commit 793bd343ad1d0242e049fd7a3187c0f593498944.

--- lib/rspec/mocks/methods.rb
+++ lib/rspec/mocks/methods.rb
@@ -72,18 +72,12 @@ module RSpec
 
     private
 
-      @@mock_proxies = Hash.new do |hash, key|
-        hash[key] = key.instance_eval do
-          if Mock === self
-            Proxy.new(self, @name, @options)
-          else
-            Proxy.new(self)
-          end
-        end
-      end
-
       def __mock_proxy
-        @@mock_proxies[self]
+        if Mock === self
+          @mock_proxy ||= Proxy.new(self, @name, @options)
+        else
+          @mock_proxy ||= Proxy.new(self)
+        end
       end
 
       def format_chain(*chain, &blk)
