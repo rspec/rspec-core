f2e2ae2198662febc02df907fdba88dd9a32ad95
Leonardo Bessa <leobessa@gmail.com>
2010-10-06 17:06:48 -0300

    ops... File.open instead of File.new

--- spec/rspec/core/option_parser_spec.rb
+++ spec/rspec/core/option_parser_spec.rb
@@ -25,13 +25,13 @@ module RSpec::Core
     end
 
     it "parses output stream from --out" do
-      File.should_receive(:new).with("foo.txt").and_return(output_file)
+      File.should_receive(:open).with("foo.txt",'w').and_return(output_file)
       options = Parser.parse!(%w[--out foo.txt])
       options.should eq( {:output_stream=>output_file} )
     end
 
     it "parses output stream from -o" do
-      File.should_receive(:new).with("foo.txt").and_return(output_file)
+      File.should_receive(:open).with("foo.txt",'w').and_return(output_file)
       options = Parser.parse!(%w[-o foo.txt])
       options.should eq( {:output_stream=>output_file} )
     end
