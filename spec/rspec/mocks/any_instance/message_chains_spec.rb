c77dbebb6bbcdf4753f78fb13c2686639242c552
David Chelimsky <dchelimsky@gmail.com>
2011-06-21 08:22:10 -0500

    ruby-1.9.1: use fully qualified names in specs

--- spec/rspec/mocks/any_instance/message_chains_spec.rb
+++ spec/rspec/mocks/any_instance/message_chains_spec.rb
@@ -1,37 +1,31 @@
 require 'spec_helper'
 
-module RSpec
-  module Mocks
-    module AnyInstance
-      describe MessageChains do
-        let(:chains) { MessageChains.new }
-        let(:stub_chain) { StubChain.new }
-        let(:expectation_chain) { ExpectationChain.new }
-        
-        it "knows if a method does not have an expectation set on it" do
-          chains.add(:method_name, stub_chain)
-          chains.has_expectation?(:method_name).should be_false
-        end
-        
-        it "knows if a method has an expectation set on it" do
-          chains.add(:method_name, stub_chain)
-          chains.add(:method_name, expectation_chain)
-          chains.has_expectation?(:method_name).should be_true
-        end
-        
-        context "creating stub chains" do
-          it "understands how to add a stub chain for a method" do
-            chains.add(:method_name, stub_chain)
-            chains[:method_name].should eq([stub_chain])
-          end
+describe RSpec::Mocks::AnyInstance::MessageChains do
+  let(:chains) { RSpec::Mocks::AnyInstance::MessageChains.new }
+  let(:stub_chain) { RSpec::Mocks::AnyInstance::StubChain.new }
+  let(:expectation_chain) { RSpec::Mocks::AnyInstance::ExpectationChain.new }
 
-          it "allows multiple stub chains for a method" do
-            chains.add(:method_name, stub_chain)
-            chains.add(:method_name, another_stub_chain = StubChain.new)
-            chains[:method_name].should eq([stub_chain, another_stub_chain])
-          end
-        end
-      end
+  it "knows if a method does not have an expectation set on it" do
+    chains.add(:method_name, stub_chain)
+    chains.has_expectation?(:method_name).should be_false
+  end
+
+  it "knows if a method has an expectation set on it" do
+    chains.add(:method_name, stub_chain)
+    chains.add(:method_name, expectation_chain)
+    chains.has_expectation?(:method_name).should be_true
+  end
+
+  context "creating stub chains" do
+    it "understands how to add a stub chain for a method" do
+      chains.add(:method_name, stub_chain)
+      chains[:method_name].should eq([stub_chain])
+    end
+
+    it "allows multiple stub chains for a method" do
+      chains.add(:method_name, stub_chain)
+      chains.add(:method_name, another_stub_chain = RSpec::Mocks::AnyInstance::StubChain.new)
+      chains[:method_name].should eq([stub_chain, another_stub_chain])
     end
   end
 end
