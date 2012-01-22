4dffaa421ced862ae7b55a93c24f21d3dd18b675
Jon Frisby <jfrisby@mrjoy.com>
2012-01-22 01:56:29 -0600

    Ruby 1.8 compatibility.

--- spec/rspec/core/gc_spec.rb
+++ spec/rspec/core/gc_spec.rb
@@ -3,16 +3,40 @@ require 'spec_helper'
 describe RSpec::Core do
 
   describe "#configuration" do
-    it "allows us to disable GC" do
-      GC.start # Encourage GC before disabling in case we're near trigger
-               # threshold -- don't want calling configure to up the GC count.
-      RSpec.configuration.gc_every_n_examples = 1 # This should call GC.disable.
-      cycles_before = GC.count
-      GC.start # This is a no-op if GC has been disabled.
-      cycles_after = GC.count
-      cycles_after.should == cycles_before
+    if(GC.respond_to?(:count))
+      it "allows us to use normal GC behavior" do
+        RSpec.configuration.gc_every_n_examples = 0 # This should call GC.enable.
 
-      # TODO: Wipe the configuration, re-enable GC, etc.
+        cycles_before = GC.count
+        GC.start # This is a no-op if GC has been disabled.
+        cycles_after = GC.count
+
+        cycles_after.should_not == cycles_before
+      end
+
+      it "allows us to disable GC" do
+        RSpec.configuration.gc_every_n_examples = 1 # This should call GC.disable.
+
+        cycles_before = GC.count
+        GC.start # This is a no-op if GC has been disabled.
+        cycles_after = GC.count
+
+        cycles_after.should == cycles_before
+      end
+
+      before do
+        # Be courteous to other test groups / global suite configuration which
+        # might want to set this option...
+        @saved_gc_config = RSpec.configuration.gc_every_n_examples
+      end
+
+      after do
+        RSpec.configuration.gc_every_n_examples = @saved_gc_config
+      end
+    else
+      it "doesn't do squat because our Ruby doesn't have GC.count" do
+        pending "Skipping GC test on Ruby that doesn't support GC.count."
+      end
     end
   end
 
