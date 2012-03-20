d6f74cd2eab0ee4dc24d6ec194cf77e7758cb31f
David Chelimsky <dchelimsky@gmail.com>
2012-03-20 06:18:19 -0500

    less indirection

--- lib/rspec/core/example_group.rb
+++ lib/rspec/core/example_group.rb
@@ -278,7 +278,7 @@ module RSpec
 
       # @private
       def self.run_around_each_hooks(example, initial_procsy)
-        example.around_hooks.inject(initial_procsy) do |procsy, around_hook|
+        around_hooks_for(example).inject(initial_procsy) do |procsy, around_hook|
           Example.procsy(procsy.metadata) do
             example.example_group_instance.instance_eval_with_args(procsy, &around_hook)
           end
