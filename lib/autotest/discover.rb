544614bbdfd4545ed4bb7bec31d8a5ca1fcb18b1
Jon Rowe <hello@jonrowe.co.uk>
2013-08-20 23:57:33 +0100

    simple warning

--- lib/autotest/discover.rb
+++ lib/autotest/discover.rb
@@ -1,1 +1,5 @@
-Autotest.add_discovery { "rspec2" } if File.exist?("./.rspec")
+if File.exist?("./.rspec")
+  Autotest.add_discovery { "rspec2" }
+else
+  warn "RSpec Autotest integration was attempted, but no `.rspec` file was present. Assuming RSpec not loaded."
+end
