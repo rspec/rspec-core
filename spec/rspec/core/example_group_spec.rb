4550ee45aab5c3fed4aab217f47a073335d72f8d
Xavier Shay <xavier@rhnh.net>
2013-11-02 14:05:22 -0700

    s/metadta/metadata/

--- spec/rspec/core/example_group_spec.rb
+++ spec/rspec/core/example_group_spec.rb
@@ -225,7 +225,7 @@ module RSpec::Core
         expect(examples_run.count).to eq(1)
       end
 
-      it "still runs matching config hooks when metadta is added later" do
+      it "still runs matching config hooks when metadata is added later" do
         examples_run = 0
         RSpec.configuration.before(:all, :type => :run_counter) { examples_run += 1 }
 
