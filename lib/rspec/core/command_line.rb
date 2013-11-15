d10d2c801a77364610ae39913310e44cd672e223
Ben Hoskings <ben@hoskings.net>
2013-11-15 13:42:11 +1100

    Parameterise the example_groups run in CommandLine.
    Libraries like test-queue run a custom list of groups per worker (in the test-queue case, the list wraps a socket that receives example groups from the master). Parameterising the list here allows a custom list to be supplied cleanly.

--- lib/rspec/core/command_line.rb
+++ lib/rspec/core/command_line.rb
@@ -17,7 +17,7 @@ module RSpec
       # @param [IO] out
       def run(err, out)
         setup(err, out)
-        run_specs
+        run_specs(@world.ordered_example_groups)
       end
 
       def setup(err, out)
@@ -28,11 +28,11 @@ module RSpec
         @world.announce_filters
       end
 
-      def run_specs
+      def run_specs(example_groups)
         @configuration.reporter.report(@world.example_count) do |reporter|
           begin
             @configuration.hooks.run(:before, :suite)
-            @world.ordered_example_groups.map {|g| g.run(reporter) }.all? ? 0 : @configuration.failure_exit_code
+            example_groups.map {|g| g.run(reporter) }.all? ? 0 : @configuration.failure_exit_code
           ensure
             @configuration.hooks.run(:after, :suite)
           end
