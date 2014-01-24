429078993a4a88a45ea9cda2a4688b1f19066f66
Jon Rowe <hello@jonrowe.co.uk>
2014-01-24 12:08:16 +1100

    send notification objects from reporter

--- lib/rspec/core/reporter.rb
+++ lib/rspec/core/reporter.rb
@@ -1,5 +1,16 @@
 module RSpec::Core
   class Reporter
+    CountNotification   = Struct.new(:count)
+    ExampleNotification = Struct.new(:example)
+    GroupNotification   = Struct.new(:group)
+    MessageNotification = Struct.new(:message)
+    SeedNotification    = Struct.new(:seed,:used) do
+      def seed_used?
+        !!used
+      end
+    end
+    SummaryNotification = Struct.new(:duration, :examples, :failures, :pending)
+    Notification        = Class.new
 
     def initialize(configuration)
       @configuration = configuration
