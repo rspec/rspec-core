e1de53d369ba70fb18108cb68c00d9b937ee8ac2
Mike Kendall <zenkalia@gmail.com>
2014-04-16 15:44:42 -0700

    :ledger: actually, the --help is generated from the options parser, so we probably want the rules collapsed like the other shortcut rules

--- lib/rspec/core/option_parser.rb
+++ lib/rspec/core/option_parser.rb
@@ -51,19 +51,11 @@ module RSpec::Core
           options[:order] = "rand:#{seed}"
         end
 
-        parser.on('--fail-fast', 'Abort the run on first failure.') do |o|
+        parser.on('--fail-fast', '--fast-fail', 'Abort the run on first failure.') do |o|
           options[:fail_fast] = true
         end
 
-        parser.on('--fast-fail', 'Abort the run on first failure.') do |o|
-          options[:fail_fast] = true
-        end
-
-        parser.on('--no-fail-fast', 'Do not abort the run on first failure.') do |o|
-          options[:fail_fast] = false
-        end
-
-        parser.on('--no-fast-fail', 'Do not abort the run on first failure.') do |o|
+        parser.on('--no-fail-fast', '--no-fast-fail', 'Do not abort the run on first failure.') do |o|
           options[:fail_fast] = false
         end
 
