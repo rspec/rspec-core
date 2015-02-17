1363de60c8321be5554020bb23d91437ae98672f
Josh Cheek <josh.cheek@gmail.com>
2015-02-17 01:59:01 -0700

    Switch ThreadOrderSupport interface
    Decided to write as if the code I wanted existed. Turns out it wasn't what I wrote.
    So deleted that stuff and wrote the thing I was trying to use. Hopefully it's worthwhile,
    but I'm not especially optimistic.
    
    Anyway, passed 20x on rbx, so I feel okay about it.
    Shit's fkn hard w/o Queues O.o

--- spec/support/thread_order.rb
+++ spec/support/thread_order.rb
@@ -1,23 +1,91 @@
 class ThreadOrderSupport
-  def initialize(errstream)
-    @mutex     = Mutex.new
-    @errstream = errstream
-    @current   = 1
+  def initialize
+    @bodies     = {}
+    @threads    = []
+    @queue      = [] # we may not have thread stdlib required, so may not have Queue class
+    @mutex      = Mutex.new
+    @worker     = Thread.new { loop { work } }
+    @worker.abort_on_exception = true
   end
 
-  def call(turn_number)
-    loop do
-      @mutex.lock
-      break if @current == turn_number
-      @mutex.unlock
-      Thread.pass
+  def declare(name, &block)
+    @bodies[name] = block
+  end
+
+  def current
+    Thread.current[:thread_order_name]
+  end
+
+  def pass_to(name, options)
+    parent       = Thread.current
+    resume_event = extract_resume_event! options
+    body         = @bodies.fetch name
+    event        = lambda do |event|
+      return unless event == sync { resume_event }
+      sync { resume_event = :has_passed }
+      parent.wakeup
     end
-    yield
-  rescue Exception => e
-    @errstream << "#{e.inspect}\n#{e.backtrace}"
-    raise
-  ensure
-    @current += 1
-    @mutex.unlock
+
+    child = Thread.new do
+      begin
+        event.call :run
+        Thread.current[:thread_order_name] = name
+        body.call
+      rescue Exception => error
+        parent.raise error
+        raise
+      ensure
+        event.call :exit
+      end
+    end
+
+    sync { @threads << child }
+
+    sync { resume_event == :sleep } &&
+      watch_for_sleep(child) { event.call :sleep }
+
+    sync { resume_event == :has_passed } ||
+      sleep
+
+    child
+  end
+
+  def apocalypse!
+    enqueue do
+      sync { @threads.each(&:kill) }
+      @queue.clear
+      @worker.kill
+    end
+  end
+
+  private
+
+  def sync(&block)
+    @mutex.synchronize(&block)
+  end
+
+  def enqueue(&block)
+    sync { @queue << block }
+  end
+
+  def work
+    task = sync { @queue.shift }
+    task ||= lambda { Thread.pass }
+    task.call
+  end
+
+  def extract_resume_event!(options)
+    resume_on = options.delete :resume_on
+    options.any? &&
+      raise(ArgumentError, "Unknown options: #{options.inspect}")
+    resume_on && ![:run, :exit, :sleep].include?(resume_on) and
+      raise(ArgumentError, "Unknown status: #{resume_on.inspect}")
+    resume_on
+  end
+
+  def watch_for_sleep(thread, &cb)
+    return if thread.status == false
+    thread.status == 'sleep' && cb.call
+    enqueue { watch_for_sleep(thread, &cb) }
   end
 end
--- spec/support_specs/thread_order_spec.rb
+++ spec/support_specs/thread_order_spec.rb
@@ -1,76 +1,114 @@
 RSpec.describe ThreadOrderSupport do
-  let!(:order)  { described_class.new $stderr }
+  let(:order) { described_class.new }
+  after { order.apocalypse! }
 
-  it 'begins turns at 1' do
-    ran = false
-    order.call(1) { ran = true }
-    expect(ran).to eq true
+  it 'allows thread behaviour to be declared and run by name' do
+    seen = []
+    order.declare(:third)  { seen << :third }
+    order.declare(:first)  { seen << :first;  order.pass_to :second, :resume_on => :exit }
+    order.declare(:second) { seen << :second; order.pass_to :third,  :resume_on => :exit }
+    expect(seen).to eq []
+    order.pass_to :first, :resume_on => :exit
+    expect(seen).to eq [:first, :second, :third]
   end
 
-  it 'runs each codeblock when it is that code\'s turn' do
-    seen    = []
-    threads = 10.downto(1).map do |i|
-      Thread.new { order.call(i) { seen << i } }
-    end
-    threads.each(&:join)
-    expect(seen).to eq (1..10).to_a
+  it 'sleeps the thread which passed' do
+    main_thread = Thread.current
+    order.declare(:thread) { :noop until main_thread.status == 'sleep' }
+    order.pass_to :thread, :resume_on => :exit # passes if it doesn't lock up
   end
 
-  it 'returns whatever the block returned' do
-    expect(order.call(1) { :block_returned }).to eq :block_returned
+  context 'resume events' do
+    def self.test_status(name, statuses, *args, &threadmaker)
+      it "can resume the thread when the called thread enters #{name}", *args do
+        thread   = instance_eval(&threadmaker)
+        statuses = Array statuses
+        expect(statuses).to include thread.status
+      end
+    end
+
+    test_status ':run', 'run' do
+      order.declare(:t) { loop { 1 } }
+      order.pass_to :t, :resume_on => :run
+    end
+
+    test_status ':sleep', 'sleep' do
+      order.declare(:t) { sleep }
+      order.pass_to :t, :resume_on => :sleep
+    end
+
+    test_status ':exit', [false, 'aborting'] do
+      order.declare(:t) { Thread.exit }
+      order.pass_to :t, :resume_on => :exit
+    end
   end
 
-  describe 'exceptions' do
-    def call(&block)
-      stream    = ""
-      exception = nil
-      begin
-        described_class.new(stream).call(1, &block)
-      rescue Exception => e
-        exception = e
-      end
-      {:stream => stream, :exception => exception}
+  describe 'errors in children' do
+    specify 'are raised in the child' do
+      child = nil
+      order.declare(:err) { child = Thread.current; raise 'the roof' }
+      (order.pass_to :err, :resume_on => :exit) rescue nil
+      child.join                                rescue nil
+      expect(child.status).to eq nil
     end
 
-    def stream_for(&block)
-      call(&block).fetch :stream
+    specify 'are raised in the parent' do
+      order.declare(:err) { raise Exception, 'to the rules' }
+      expect {
+        order.pass_to :err, :resume_on => :run
+        loop { :noop }
+      }.to raise_error Exception, 'to the rules'
     end
 
-    def error_for(&block)
-      call(&block).fetch :exception
+    specify 'even if the parent is asleep' do
+      parent = Thread.current
+      order.declare(:err) {
+        :noop until parent.status == 'sleep'
+        raise 'the roof'
+      }
+      expect {
+        order.pass_to :err, :resume_on => :run
+        sleep
+      }.to raise_error RuntimeError, 'the roof'
+    end
+  end
+
+  it 'knows which thread is running' do
+    thread_names = []
+    order.declare(:a) {
+      thread_names << order.current
+      order.pass_to :b, :resume_on => :exit
+      thread_names << order.current
+    }
+    order.declare(:b) {
+      thread_names << order.current
+    }
+    order.pass_to :a, :resume_on => :exit
+    expect(thread_names.sort).to eq [:a, :a, :b]
+  end
+
+  it 'returns nil when asked for the current thread by one it did not define' do
+    thread_names = []
+    order.declare(:a) {
+      thread_names << order.current
+      Thread.new { thread_names << order.current }.join
+    }
+    expect(order.current).to eq nil
+    order.pass_to :a, :resume_on => :exit
+    expect(thread_names).to eq [:a, nil]
+  end
+
+  describe 'incorrect interface usage' do
+    it 'raises ArgumentError when told to resume on an unknown status' do
+      order.declare(:t) { }
+      expect { order.pass_to :t, :resume_on => :bad_status }.
+        to raise_error(ArgumentError, /bad_status/)
     end
 
-    it 'prints the class to the error stream' do
-      expect(stream_for { raise ZeroDivisionError }).to include 'ZeroDivisionError'
-    end
-
-    it 'prints the message to the error stream' do
-      expect(stream_for { raise 'the message' }).to include 'the message'
-    end
-
-    it 'prints the backtrace to the error stream' do
-      expect(stream_for { raise }).to include "#{__FILE__}:#{__LINE__}"
-    end
-
-    it 'reraises the exception with the original backtrace' do
-      error = error_for do
-        raise # place on its own line to avoid false positive from the method invocation
-      end
-      expect(error.backtrace).to be_any { |line| line.include?  "#{__FILE__}:#{__LINE__ - 2}" }
-    end
-
-    it 'does this for all exception types, even Exception' do
-      error = error_for { raise Exception }
-      expect(error.class).to eq Exception
-    end
-
-    it 'considers this a valid turn and moves onto the next one' do
-      order = described_class.new ""
-      begin
-        order.call(1) { raise ZeroDivisionError }
-      rescue ZeroDivisionError
-      end
-      order.call(2) { } # shouldn't block/deadlock
+    it 'raises an ArgumentError when you give it unknown keys (ie you spelled resume_on wrong)' do
+      order.declare(:t) { }
+      expect { order.pass_to :t, :bad_key => :t }.
+        to raise_error(ArgumentError, /bad_key/)
     end
   end
 end
