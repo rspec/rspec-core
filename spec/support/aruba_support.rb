b761e78dbd8e3acfbde462db39d99508028def6e
jackscotti <jacopojackscotti@gmail.com>
2015-07-17 11:50:35 +0100

    Remove deprecated in_current_dir
    Replaced with in_current_directory

--- spec/support/aruba_support.rb
+++ spec/support/aruba_support.rb
@@ -23,7 +23,7 @@ RSpec.shared_context "aruba support" do
     cmd_parts = Shellwords.split(cmd)
 
     handle_current_dir_change do
-      in_current_dir do
+      in_current_directory do
         RSpec::Core::Runner.run(cmd_parts, temp_stderr, temp_stdout)
       end
     end
