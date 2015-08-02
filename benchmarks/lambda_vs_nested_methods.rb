13b482ab0b063c43d6e9890e0154cb3df217bb96
bennacer860 <bennacer860@gmail.com>
2015-08-02 10:20:06 -0400

    add benchmark result to the file

--- benchmarks/lambda_vs_nested_methods.rb
+++ benchmarks/lambda_vs_nested_methods.rb
@@ -21,3 +21,16 @@ Benchmark.ips do |x|
 
   x.compare!
 end
+
+__END__
+
+Calculating -------------------------------------
+      nested methods    47.146k i/100ms
+              lambda    85.962k i/100ms
+-------------------------------------------------
+      nested methods    731.076k (± 6.2%) i/s -      3.677M
+              lambda      1.711M (± 4.5%) i/s -      8.596M
+
+Comparison:
+              lambda:  1711389.0 i/s
+      nested methods:   731075.7 i/s - 2.34x slower
