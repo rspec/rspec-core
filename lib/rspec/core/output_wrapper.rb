046179ca43bf96964a51ff645a5007d650e6c742
Myron Marston <myron.marston@gmail.com>
2017-05-03 16:55:47 -0700

    Do not allow `output_wrapper` to wrap a non-output object.

--- lib/rspec/core/output_wrapper.rb
+++ lib/rspec/core/output_wrapper.rb
@@ -1,27 +1,36 @@
+RSpec::Support.require_rspec_support "directory_maker"
+
 module RSpec
   module Core
     # @private
     class OutputWrapper
       # @private
-      attr_accessor :output
+      attr_reader :output
 
       # @private
-      def initialize(output)
-        @output = output
+      def initialize(io_or_file_path)
+        @output = open_stream(io_or_file_path)
       end
 
-      def respond_to?(name, priv=false)
-        output.respond_to?(name, priv)
-      end
-
-      def method_missing(name, *args, &block)
-        output.method_missing(name, *args, &block)
+      def output=(io_or_file_path)
+        @output = open_stream(io_or_file_path)
       end
 
       # Redirect calls for IO interface methods
       IO.instance_methods(false).each do |method|
         define_method(method) do |*args, &block|
           output.send(method, *args, &block)
+        end
+      end
+
+    private
+
+      def open_stream(io_or_file_path)
+        if io_or_file_path.respond_to?(:puts)
+          io_or_file_path
+        else
+          RSpec::Support::DirectoryMaker.mkdir_p(File.dirname(io_or_file_path))
+          File.open(io_or_file_path, 'w')
         end
       end
     end
