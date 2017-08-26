52fff77fb251eb67736d5fd614d5053c97d3e375
Pat Allan <pat@freelancing-gods.com>
2017-08-26 14:57:19 +0200

    Further Aruba syntax updates.

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,13 +12,13 @@ Before do
 end
 
 Aruba.configure do |config|
-  config.before_cmd do |cmd|
+  config.before(:command) do |cmd|
     set_environment_variable('JRUBY_OPTS', "-X-C #{ENV['JRUBY_OPTS']}") # disable JIT since these processes are so short lived
   end
 end if RUBY_PLATFORM == 'java'
 
 Aruba.configure do |config|
-  config.before_cmd do |cmd|
+  config.before(:command) do |cmd|
     set_environment_variable('RBXOPT', "-Xint=true #{ENV['RBXOPT']}") # disable JIT since these processes are so short lived
   end
 end if defined?(Rubinius)
