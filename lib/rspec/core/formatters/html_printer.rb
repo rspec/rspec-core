e597c4c3cc201df9d747c34f10de0175cd725491
Garett Arrowood <garettarrowood@gmail.com>
2018-01-20 09:30:16 -0500

    Make necessary changes for green CI

--- lib/rspec/core/formatters/html_printer.rb
+++ lib/rspec/core/formatters/html_printer.rb
@@ -33,10 +33,8 @@ module RSpec
             "<span class='duration'>#{formatted_run_time}s</span></dd>"
         end
 
-        # rubocop:disable Metrics/ParameterLists
         def print_example_failed(pending_fixed, description, run_time, failure_id,
                                  exception, extra_content)
-          # rubocop:enable Metrics/ParameterLists
           formatted_run_time = "%.5f" % run_time
 
           @output.puts "    <dd class=\"example #{pending_fixed ? 'pending_fixed' : 'failed'}\">"
