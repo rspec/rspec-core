9e8833cb45112b013d181ccb54963183678d510c
Chris Drane <obromois@gmail.com>
2019-02-27 20:38:33 +1100

    Change algorithm to use ::DidYouMean::SpellChecker API

--- lib/rspec/core/did_you_mean.rb
+++ lib/rspec/core/did_you_mean.rb
@@ -1,36 +1,32 @@
 module RSpec
   module Core
     # Service object to provide did_you_mean suggestions
-    # based on https://github.com/yuki24/did_you_mean
     class DidYouMean
-      CUT_OFF = 0.15 # Lowest acceptable nearness to be considered probable
-      MAX_SUGGESTIONS = 3 # Maximum number of suggestions that can be provided.
-      attr_reader :relative_file_name, :exception
+      attr_reader :relative_file_name
 
       def initialize(relative_file_name)
         @relative_file_name = relative_file_name
       end
 
-      if String.method_defined?(:codepoints)
+      if defined?(::DidYouMean::SpellChecker)
         # provide probable suggestions if a LoadError
         def call
-          probables = find_probables
-
+          checker = ::DidYouMean::SpellChecker.new(:dictionary => Dir["spec/**/*.rb"])
+          probables = checker.correct(relative_file_name)
           return unless probables.any?
 
-          short_list = probables.sort_by { |_, proximity| proximity }[0...MAX_SUGGESTIONS]
-          formats short_list
+          formats probables
         end
-      else # ruby 1.9.2 or less
-        # return nil if UTF-8 not fully supported
+      else # ruby 2.3.2 or less
+        # return nil if API for ::DidYouMean::SpellChecker not supported
         def call
         end
       end
 
       private
 
-      def formats(short_list)
-        rspec_format = short_list.map { |s, _| "rspec ./#{s}" }
+      def formats(probables)
+        rspec_format = probables.map { |s, _| "rspec ./#{s}" }
         red_font(top_and_tail rspec_format)
       end
 
@@ -39,49 +35,9 @@ module RSpec
         rspec_format.insert(0, ' - Did you mean?').join("\n#{spaces}") + "\n"
       end
 
-      def find_probables
-        possibilities = Dir["spec/**/*.rb"]
-        name = relative_file_name.sub('./', '')
-        possibilities.map do |possible|
-          [possible,  proximity(possible, name)] if proximity(possible, name) <= CUT_OFF
-        end.compact
-      end
-
       def red_font(mytext)
         colorizer = ::RSpec::Core::Formatters::ConsoleCodes
         colorizer.wrap mytext, :failure
-      end
-
-      # based on
-      # https://github.com/ioquatix/build-text/blob/master/lib/build/text/merge.rb
-      def levenshtein_distance(str1, str2)
-        n = str1.length
-        m = str2.length
-        return m if n == 0
-        return n if m == 0
-        d = (0..m).to_a
-        x = nil
-        n.times do |i|
-          e = i + 1
-          m.times do |j|
-            cost = (str1[i] == str2[j]) ? 0 : 1
-            x = [
-              d[j + 1] + 1, # insertion
-              e + 1,      # deletion
-              d[j] + cost # substitution
-            ].min
-            d[j] = e
-            e = x
-          end
-          d[m] = x
-        end
-        return x
-      end
-
-      def proximity(str1, str2)
-        distance = levenshtein_distance(str1, str2)
-        average_length = (str1.length + str2.length) / 2.0
-        distance.to_f / average_length
       end
     end
   end
--- spec/rspec/core/did_you_mean_spec.rb
+++ spec/rspec/core/did_you_mean_spec.rb
@@ -4,7 +4,7 @@ require 'rspec/support/spec/in_sub_process'
 module RSpec
   module Core
     RSpec.describe DidYouMean do
-      if String.method_defined?(:codepoints)
+      if defined?(::DidYouMean::SpellChecker)
         describe 'Call to DidYouMean' do
           describe 'Success' do
             let(:name) { './spec/rspec/core/did_you_mean_spec.rb' }
@@ -15,7 +15,7 @@ module RSpec
               it 'should only return a small number of suggestions' do
                 name = './spec/rspec/core/drb_spec.r'
                 suggestions = DidYouMean.new(name).call
-                expect(suggestions.split("\n").size).to eq 3
+                expect(suggestions.split("\n").size).to eq 2
               end
             end
           end
@@ -26,25 +26,7 @@ module RSpec
             end
           end
         end
-        describe 'Key private methods' do
-          let(:str1) { 'canonical' }
-          describe 'levenshtein_distance' do
-            it{ expect(DidYouMean.new(str1).send(:levenshtein_distance, str1, test_h[:empty])).to eq 9 }
-            it{ expect(DidYouMean.new(str1).send(:levenshtein_distance, str1, test_h[:identical])).to eq 0 }
-            it{ expect(DidYouMean.new(str1).send(:levenshtein_distance, str1, test_h[:two_insertions])).to eq 2 }
-            it{ expect(DidYouMean.new(str1).send(:levenshtein_distance, str1, test_h[:two_insertions_deletion])).to eq 3 }
-            it{ expect(DidYouMean.new(str1).send(:levenshtein_distance, str1, test_h[:insertion_substitution_deletion])).to eq 3 }
-            context 'UTF-8' do
-              let(:str1) { '變形金剛4:絕跡重生' }
-              it{ expect(DidYouMean.new(str1).send(:levenshtein_distance, str1,
-                '變形金剛4: 絕跡重生')).to eq 1 }
-            end
-          end
-          describe 'proximity' do
-            it{ expect(DidYouMean.new(str1).send(:proximity, str1, test_h[:two_insertions])).to eq 0.2 }
-          end
-        end
-      else # ruby 1.9.2 or less
+      else # ruby 2.3.2 or less
         describe 'Call to DidYouMean' do
           describe 'Success' do
             let(:name) { './spec/rspec/core/did_you_mean_spec.rb' }
@@ -53,17 +35,6 @@ module RSpec
             end
           end
         end
-      end
-
-      private
-      def test_h
-        {
-          empty: '',
-          identical: 'canonical',
-          two_insertions: 'canonixxcal',
-          two_insertions_deletion: 'anonixxcal',
-          insertion_substitution_deletion: 'fnaonical'
-        }
       end
     end
   end
