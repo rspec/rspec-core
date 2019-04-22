6949308814d286217606551e76e3f1c2392b16ff
Romain Tartière <romain@blogreen.org>
2019-04-22 12:41:53 -1000

    Discard messages as well as profiling information
    While here, add a note stating that this message discarding is an
    expected behavior.

--- lib/rspec/core/formatters/minimal_formatter.rb
+++ lib/rspec/core/formatters/minimal_formatter.rb
@@ -5,13 +5,18 @@ module RSpec
     module Formatters
       # @private
       class MinimalFormatter < BaseFormatter
-        Formatters.register self, :example_failed, :dump_profile
+        Formatters.register self, :example_failed, :dump_profile, :message
 
         def example_failed(failure)
           output.puts "#{failure.example.location}:#{failure.example.description}"
         end
 
+        # Discard profile and messages
+        #
+        # These outputs are not really relevant in the context of this minimal
+        # formatter.
         def dump_profile(_profile); end
+        def message(_message); end
       end
     end
   end
