82928aac51e1e7a06f155f73a72d6a5a88e7b2a8
Jon Rowe <hello@jonrowe.co.uk>
2019-10-08 10:06:57 +0100

    Version 3.9.0

--- lib/rspec/core/version.rb
+++ lib/rspec/core/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Core.
     module Version
       # Current version of RSpec Core, in semantic versioning format.
-      STRING = '3.9.0.pre'
+      STRING = '3.10.0.pre'
     end
   end
 end
