abbbd37d3346c580d83ecb96819e29d063d97852
Jon Rowe <hello@jonrowe.co.uk>
2019-12-27 21:22:21 +0000

    Merge pull request #2680 from rspec/update-travis-build-scripts-2019-12-26-for-master
    Ruby 2.7 support

--- spec/support/helper_methods.rb
+++ spec/support/helper_methods.rb
@@ -14,24 +14,32 @@ module RSpecHelpers
     result
   end
 
-  def with_safe_set_to_level_that_triggers_security_errors
-    result = nil
-
-    orig_safe = $SAFE
-    Thread.new do
-      ignoring_warnings { $SAFE = SAFE_LEVEL_THAT_TRIGGERS_SECURITY_ERRORS }
-      result = yield
-    end.join
-
-    # $SAFE is not supported on Rubinius
-    # In Ruby 2.6, $SAFE became a global variable; previously it was local to a thread.
-    unless defined?(Rubinius) || SAFE_IS_GLOBAL_VARIABLE
-      # $SAFE should not have changed in this thread.
-      expect($SAFE).to eql orig_safe
+  # In Ruby 2.7 taint was removed and has no effect, whilst SAFE warns that it
+  # has no effect and will become a normal varible in 3.0.
+  if RUBY_VERSION >= '2.7'
+    def with_safe_set_to_level_that_triggers_security_errors
+      yield
     end
+  else
+    def with_safe_set_to_level_that_triggers_security_errors
+      result = nil
 
-    result
-  ensure
-    $SAFE = orig_safe if orig_safe && SAFE_IS_GLOBAL_VARIABLE
+      orig_safe = $SAFE
+      Thread.new do
+        ignoring_warnings { $SAFE = SAFE_LEVEL_THAT_TRIGGERS_SECURITY_ERRORS }
+        result = yield
+      end.join
+
+      # $SAFE is not supported on Rubinius
+      # In Ruby 2.6, $SAFE became a global variable; previously it was local to a thread.
+      unless defined?(Rubinius) || SAFE_IS_GLOBAL_VARIABLE
+        # $SAFE should not have changed in this thread.
+        expect($SAFE).to eql orig_safe
+      end
+
+      result
+    ensure
+      $SAFE = orig_safe if orig_safe && SAFE_IS_GLOBAL_VARIABLE
+    end
   end
 end
