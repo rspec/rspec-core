909cb35e213d0f57f116718e5391204a2d043f8b
Patrik Wenger <patrik.wenger@mindclue.ch>
2021-01-01 07:13:48 +0100

    FailureListFormatter: use exception message as reason, not spec name

--- lib/rspec/core/formatters/failure_list_formatter.rb
+++ lib/rspec/core/formatters/failure_list_formatter.rb
@@ -8,7 +8,18 @@ module RSpec
         Formatters.register self, :example_failed, :dump_profile, :message
 
         def example_failed(failure)
-          output.puts "#{failure.example.location}:#{failure.example.description}"
+          if failure.exception.respond_to?(:all_exceptions)
+            exceptions = failure.exception.all_exceptions
+          else
+            exceptions = [failure.exception]
+          end
+
+          exceptions.each do |exception|
+            reason   = exception.message.lines.map(&:strip).reject(&:empty?).join ' '
+            location = find_relevant_location failure, exception
+
+            output.puts "#{location}:#{reason}"
+          end
         end
 
         # Discard profile and messages
@@ -17,6 +28,28 @@ module RSpec
         # list formatter.
         def dump_profile(_profile); end
         def message(_message); end
+
+        private
+
+        # @return [String] relevant locaton
+        def find_relevant_location(failure, exception)
+          bt = exception.backtrace
+
+          if bt && (line = bt.find { |l| backtrace_exclusion_patterns !~ l })
+            line[/[^:]+:\d+/]
+          else
+            failure.example.location
+          end
+        end
+
+        def backtrace_exclusion_patterns
+          @backtrace_exclusion_patterns ||=
+            begin
+              # ERB/eval lines
+              non_file = /^\(\w+\):/
+              Regexp.union(*RSpec.configuration.backtrace_exclusion_patterns, non_file)
+            end
+        end
       end
     end
   end
--- spec/rspec/core/formatters/failure_list_formatter_spec.rb
+++ spec/rspec/core/formatters/failure_list_formatter_spec.rb
@@ -7,12 +7,13 @@ module RSpec::Core::Formatters
     it 'produces the expected full output' do
       output = run_example_specs_with_formatter('failures')
       expect(output).to eq(<<-EOS.gsub(/^\s+\|/, ''))
-        |./spec/rspec/core/resources/formatter_specs.rb:4:is marked as pending but passes
-        |./spec/rspec/core/resources/formatter_specs.rb:36:fails
-        |./spec/rspec/core/resources/formatter_specs.rb:40:fails twice
-        |./spec/rspec/core/resources/formatter_specs.rb:47:fails with a backtrace that has no file
-        |./spec/rspec/core/resources/formatter_specs.rb:53:fails with a backtrace containing an erb file
-        |./spec/rspec/core/resources/formatter_specs.rb:71:raises
+        |./spec/rspec/core/resources/formatter_specs.rb:4:Expected example to fail since it is pending, but it passed.
+        |/home/roadster/dev/oss/rspec-core/spec/rspec/core/resources/formatter_specs.rb:37:expected: 2 got: 1 (compared using ==)
+        |/home/roadster/dev/oss/rspec-core/spec/rspec/core/resources/formatter_specs.rb:41:expected: 2 got: 1 (compared using ==)
+        |/home/roadster/dev/oss/rspec-core/spec/rspec/core/resources/formatter_specs.rb:42:expected: 4 got: 3 (compared using ==)
+        |/home/roadster/dev/oss/rspec-core/spec/rspec/core/resources/formatter_specs.rb:50:foo
+        |/foo.html.erb:1:Exception
+        |./spec/rspec/core/resources/formatter_specs.rb:71:boom
       EOS
     end
   end
