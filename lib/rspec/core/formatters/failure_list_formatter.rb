7aa81f4495d2264189879140f697a30db92e7a0b
Patrik Wenger <patrik.wenger@mindclue.ch>
2021-01-05 00:49:44 +0100

    FailureListFormatter: include backtrace as informational lines if --backtrace given

--- lib/rspec/core/formatters/failure_list_formatter.rb
+++ lib/rspec/core/formatters/failure_list_formatter.rb
@@ -8,17 +8,16 @@ module RSpec
         Formatters.register self, :example_failed, :dump_profile, :message
 
         def example_failed(failure)
-          if failure.exception.respond_to?(:all_exceptions)
-            exceptions = failure.exception.all_exceptions
-          else
-            exceptions = [failure.exception]
-          end
+          exceptions(failure).each do |ex|
+            locations = find_locations ex
 
-          exceptions.each do |exception|
-            reason   = exception.message.lines.map(&:strip).reject(&:empty?).join ' '
-            location = find_location(exception) || failure.example.location
-
-            output.puts "#{location}:E:#{reason}"
+            if locations.empty?
+              output.puts "#{failure.example.location}:E:#{one_line ex}"
+            else
+              locations.each do |location|
+                output.puts location
+              end
+            end
           end
         end
 
@@ -31,26 +30,51 @@ module RSpec
 
         private
 
-        # @return [String] relevant location with relative path, if possible
-        # @return [nil] if no location could be found
-        def find_location(exception)
-          require 'pathname'
-
-          bt       = exception.backtrace or return
-          exclude  = backtrace_exclusion_patterns
-          bt_line  = bt.find { |l| exclude !~ l } or return
-          md       = bt_line.match(/^(.+?):(\d+):/) or return
-          path, nr = Pathname.new(md[1]), md[2]
-
-          if path.absolute? && path.to_s.start_with?(Dir.pwd)
-            rel_path = path.relative_path_from(Pathname.pwd).to_s
-            rel_path = './' + rel_path unless rel_path =~ %r{^\.\.?/}
-            "#{rel_path}:#{nr}"
+        # @return [Array<Exception>]
+        def exceptions(failure)
+          if failure.exception.respond_to?(:all_exceptions)
+            failure.exception.all_exceptions
           else
-            "#{path}:#{nr}"
+            [failure.exception]
           end
         end
 
+        # @return [String]
+        def one_line(exception)
+          exception.message.lines.map(&:strip).reject(&:empty?).join ' '
+        end
+
+        # @return [Array<String>] relevant location with relative path, if any
+        def find_locations(exception)
+          require 'pathname'
+
+          bt        = exception.backtrace or return []
+          exclude   = backtrace_exclusion_patterns
+          bt_lines  = bt.reject { |l| exclude =~ l } or return []
+          locations = []
+
+          bt_lines.each do |bt_line|
+            md            = bt_line.match(/^(.+?):(\d+):(.*)/) or next
+            path, nr, loc = Pathname.new(md[1]), md[2], md[3]
+
+            if path.absolute? && path.to_s.start_with?(Dir.pwd)
+              path = path.relative_path_from(Pathname.pwd).to_s
+              path = './' + path unless path =~ %r{^\.\.?/}
+            end
+
+            if locations.empty?
+              locations << "#{path}:#{nr}:E:#{one_line exception}"
+            else
+              locations << "#{path}:#{nr}:I:#{loc}"
+            end
+
+            break unless RSpec.configuration.full_backtrace?
+          end
+
+          locations
+        end
+
+        # @return [Regexp]
         def backtrace_exclusion_patterns
           Regexp.union(
             *RSpec.configuration.backtrace_exclusion_patterns,
--- spec/rspec/core/formatters/failure_list_formatter_spec.rb
+++ spec/rspec/core/formatters/failure_list_formatter_spec.rb
@@ -4,17 +4,62 @@ module RSpec::Core::Formatters
   RSpec.describe FailureListFormatter do
     include FormatterSupport
 
-    it 'produces the expected full output' do
-      output = run_example_specs_with_formatter('failures')
-      expect(output).to eq(<<-EOS.gsub(/^\s+\|/, ''))
+    context 'with full backtraces disabled' do
+      before do
+        allow(RSpec.configuration).to receive(:full_backtrace?) { false }
+      end
+
+      it 'produces the expected full output' do
+        output = run_example_specs_with_formatter('failures')
+        expect(output).to eq(<<-EOS.gsub(/^\s+\|/, ''))
+          |./spec/rspec/core/resources/formatter_specs.rb:4:E:Expected example to fail since it is pending, but it passed.
+          |./spec/rspec/core/resources/formatter_specs.rb:37:E:expected: 2 got: 1 (compared using ==)
+          |./spec/rspec/core/resources/formatter_specs.rb:41:E:expected: 2 got: 1 (compared using ==)
+          |./spec/rspec/core/resources/formatter_specs.rb:42:E:expected: 4 got: 3 (compared using ==)
+          |./spec/rspec/core/resources/formatter_specs.rb:50:E:foo
+          |/foo.html.erb:1:E:Exception
+          |./spec/rspec/core/resources/formatter_specs.rb:71:E:boom
+        EOS
+      end
+    end
+
+    context 'with full backtraces enabled' do
+      before do
+        allow(RSpec.configuration).to receive(:full_backtrace?) { true }
+      end
+
+      it 'produces the expected full output' do
+        output = run_example_specs_with_formatter('failures')
+        expect(output).to eq(<<-EOS.gsub(/^\s+\|/, ''))
         |./spec/rspec/core/resources/formatter_specs.rb:4:E:Expected example to fail since it is pending, but it passed.
         |./spec/rspec/core/resources/formatter_specs.rb:37:E:expected: 2 got: 1 (compared using ==)
+        |./spec/support/formatter_support.rb:41:I:in `run_rspec_with_formatter'
+        |./spec/support/formatter_support.rb:3:I:in `run_example_specs_with_formatter'
+        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `block (3 levels) in <module:Formatters>'
+        |./spec/support/sandboxing.rb:16:I:in `block (3 levels) in <top (required)>'
+        |./spec/support/sandboxing.rb:7:I:in `block (2 levels) in <top (required)>'
         |./spec/rspec/core/resources/formatter_specs.rb:41:E:expected: 2 got: 1 (compared using ==)
+        |./spec/support/formatter_support.rb:41:I:in `run_rspec_with_formatter'
+        |./spec/support/formatter_support.rb:3:I:in `run_example_specs_with_formatter'
+        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `block (3 levels) in <module:Formatters>'
+        |./spec/support/sandboxing.rb:16:I:in `block (3 levels) in <top (required)>'
+        |./spec/support/sandboxing.rb:7:I:in `block (2 levels) in <top (required)>'
         |./spec/rspec/core/resources/formatter_specs.rb:42:E:expected: 4 got: 3 (compared using ==)
+        |./spec/support/formatter_support.rb:41:I:in `run_rspec_with_formatter'
+        |./spec/support/formatter_support.rb:3:I:in `run_example_specs_with_formatter'
+        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `block (3 levels) in <module:Formatters>'
+        |./spec/support/sandboxing.rb:16:I:in `block (3 levels) in <top (required)>'
+        |./spec/support/sandboxing.rb:7:I:in `block (2 levels) in <top (required)>'
         |./spec/rspec/core/resources/formatter_specs.rb:50:E:foo
+        |./spec/support/formatter_support.rb:41:I:in `run_rspec_with_formatter'
+        |./spec/support/formatter_support.rb:3:I:in `run_example_specs_with_formatter'
+        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `block (3 levels) in <module:Formatters>'
+        |./spec/support/sandboxing.rb:16:I:in `block (3 levels) in <top (required)>'
+        |./spec/support/sandboxing.rb:7:I:in `block (2 levels) in <top (required)>'
         |/foo.html.erb:1:E:Exception
         |./spec/rspec/core/resources/formatter_specs.rb:71:E:boom
-      EOS
+        EOS
+      end
     end
   end
 end
