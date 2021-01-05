01cb8701358475b5214f120b3f190bcc024aa109
Patrik Wenger <patrik.wenger@mindclue.ch>
2021-01-05 01:07:50 +0100

    ignore JRuby differences in backtraces

--- spec/rspec/core/formatters/failure_list_formatter_spec.rb
+++ spec/rspec/core/formatters/failure_list_formatter_spec.rb
@@ -30,32 +30,33 @@ module RSpec::Core::Formatters
 
       it 'produces the expected full output' do
         output = run_example_specs_with_formatter('failures')
+        output.gsub!(/:in `.*'$/, ":in `...'") # JRuby dumps block nesting slightly differently
         expect(output).to eq(<<-EOS.gsub(/^\s+\|/, ''))
         |./spec/rspec/core/resources/formatter_specs.rb:4:E:Expected example to fail since it is pending, but it passed.
         |./spec/rspec/core/resources/formatter_specs.rb:37:E:expected: 2 got: 1 (compared using ==)
-        |./spec/support/formatter_support.rb:41:I:in `run_rspec_with_formatter'
-        |./spec/support/formatter_support.rb:3:I:in `run_example_specs_with_formatter'
-        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `block (3 levels) in <module:Formatters>'
-        |./spec/support/sandboxing.rb:16:I:in `block (3 levels) in <top (required)>'
-        |./spec/support/sandboxing.rb:7:I:in `block (2 levels) in <top (required)>'
+        |./spec/support/formatter_support.rb:41:I:in `...'
+        |./spec/support/formatter_support.rb:3:I:in `...'
+        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `...'
+        |./spec/support/sandboxing.rb:16:I:in `...'
+        |./spec/support/sandboxing.rb:7:I:in `...'
         |./spec/rspec/core/resources/formatter_specs.rb:41:E:expected: 2 got: 1 (compared using ==)
-        |./spec/support/formatter_support.rb:41:I:in `run_rspec_with_formatter'
-        |./spec/support/formatter_support.rb:3:I:in `run_example_specs_with_formatter'
-        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `block (3 levels) in <module:Formatters>'
-        |./spec/support/sandboxing.rb:16:I:in `block (3 levels) in <top (required)>'
-        |./spec/support/sandboxing.rb:7:I:in `block (2 levels) in <top (required)>'
+        |./spec/support/formatter_support.rb:41:I:in `...'
+        |./spec/support/formatter_support.rb:3:I:in `...'
+        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `...'
+        |./spec/support/sandboxing.rb:16:I:in `...'
+        |./spec/support/sandboxing.rb:7:I:in `...'
         |./spec/rspec/core/resources/formatter_specs.rb:42:E:expected: 4 got: 3 (compared using ==)
-        |./spec/support/formatter_support.rb:41:I:in `run_rspec_with_formatter'
-        |./spec/support/formatter_support.rb:3:I:in `run_example_specs_with_formatter'
-        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `block (3 levels) in <module:Formatters>'
-        |./spec/support/sandboxing.rb:16:I:in `block (3 levels) in <top (required)>'
-        |./spec/support/sandboxing.rb:7:I:in `block (2 levels) in <top (required)>'
+        |./spec/support/formatter_support.rb:41:I:in `...'
+        |./spec/support/formatter_support.rb:3:I:in `...'
+        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `...'
+        |./spec/support/sandboxing.rb:16:I:in `...'
+        |./spec/support/sandboxing.rb:7:I:in `...'
         |./spec/rspec/core/resources/formatter_specs.rb:50:E:foo
-        |./spec/support/formatter_support.rb:41:I:in `run_rspec_with_formatter'
-        |./spec/support/formatter_support.rb:3:I:in `run_example_specs_with_formatter'
-        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `block (3 levels) in <module:Formatters>'
-        |./spec/support/sandboxing.rb:16:I:in `block (3 levels) in <top (required)>'
-        |./spec/support/sandboxing.rb:7:I:in `block (2 levels) in <top (required)>'
+        |./spec/support/formatter_support.rb:41:I:in `...'
+        |./spec/support/formatter_support.rb:3:I:in `...'
+        |./spec/rspec/core/formatters/failure_list_formatter_spec.rb:32:I:in `...'
+        |./spec/support/sandboxing.rb:16:I:in `...'
+        |./spec/support/sandboxing.rb:7:I:in `...'
         |/foo.html.erb:1:E:Exception
         |./spec/rspec/core/resources/formatter_specs.rb:71:E:boom
         EOS
