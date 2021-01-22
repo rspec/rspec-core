cf88d4e61dbc747a595d4b0a86ea94b914ec73ae
Jon Rowe <hello@jonrowe.co.uk>
2021-01-22 18:13:35 +0000

    Updated ci build scripts (from rspec-dev)main

--- script/ci_functions.sh
+++ script/ci_functions.sh
@@ -1,4 +1,4 @@
-# This file was generated on 2021-01-22T09:55:42+00:00 from the rspec-dev repo.
+# This file was generated on 2021-01-22T18:13:35+00:00 from the rspec-dev repo.
 # DO NOT modify it by hand as your changes will get lost the next time it is generated.
 
 # Taken from:
