428974abacde909ea991db322a86d190d57cd9ed
Phil Pirozhkov <hello@fili.pp.ru>
2021-02-13 13:12:55 +0300

    Change the pending with block error message
    We used to mention RSpec 2 there which is long gone.
    
    Also, this note was misleading:
    
    > If you want to declare an example `pending` and bypass the `before`
    hooks as well, you can pass `:pending => true`
    
    In the following spec:
    
        RSpec.describe do
          before { puts 'before!' }
    
          it do
            pending 'oops'
            fail 'djosdkfjos'
          end
    
          it 'pending metadata', pending: true do
            fail 'dferrerere'
          end
        end
    
    the hook is run for both metadata and in-example `pending`:
    
        before!
          pending metadata (PENDING: No reason given)
        before!
          example at ./spec/a_spec.rb:4 (PENDING: oops)

--- lib/rspec/core/pending.rb
+++ lib/rspec/core/pending.rb
@@ -72,15 +72,7 @@ module RSpec
 
         if block_given?
           raise ArgumentError, <<-EOS.gsub(/^\s+\|/, '')
-            |The semantics of `RSpec::Core::Pending#pending` have changed in
-            |RSpec 3. In RSpec 2.x, it caused the example to be skipped. In
-            |RSpec 3, the rest of the example is still run but is expected to
-            |fail, and will be marked as a failure (rather than as pending) if
-            |the example passes.
-            |
-            |Passing a block within an example is now deprecated. Marking the
-            |example as pending provides the same behavior in RSpec 3 which was
-            |provided only by the block in RSpec 2.x.
+            |Passing a block within an example is not supported.
             |
             |Move the code in the block provided to `pending` into the rest of
             |the example body.
