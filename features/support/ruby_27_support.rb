1e94b7d83bdf6d137b685425460b0363b105ac7f
Matijs van Zuijlen <matijs@matijs.net>
2021-02-28 11:56:54 +0100

    Fix deprecation

--- features/support/ruby_27_support.rb
+++ features/support/ruby_27_support.rb
@@ -1,6 +1,6 @@
 Before "@ruby-2-7" do |scenario|
   unless RUBY_VERSION.to_f == 2.7
-    warn "Skipping scenario #{scenario.title} on Ruby v#{RUBY_VERSION}"
+    warn "Skipping scenario #{scenario.name} on Ruby v#{RUBY_VERSION}"
     skip_this_scenario
   end
 end
