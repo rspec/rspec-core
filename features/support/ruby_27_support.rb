fd21f207578403a87a0ca7d5cd95c936c204f1ab
Matijs van Zuijlen <matijs@matijs.net>
2021-02-28 11:59:38 +0100

    Use regular Cucumber scenario skipping method

--- features/support/ruby_27_support.rb
+++ features/support/ruby_27_support.rb
@@ -1,7 +1,6 @@
-Around "@ruby-2-7" do |scenario, block|
-  if RUBY_VERSION.to_f == 2.7
-    block.call
-  else
+Before "@ruby-2-7" do |scenario|
+  unless RUBY_VERSION.to_f == 2.7
     warn "Skipping scenario #{scenario.title} on Ruby v#{RUBY_VERSION}"
+    skip_this_scenario
   end
 end
