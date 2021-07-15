3f6e26c7cec16b28e0b565ff9d37a36b24d743e3
Jon Rowe <hello@jonrowe.co.uk>
2021-07-15 20:47:17 +0100

    Skip scenario that fails mostly on jruby 17

--- features/support/jruby.rb
+++ features/support/jruby.rb
@@ -2,3 +2,8 @@ Around "@broken-on-jruby-9000" do |scenario, block|
   require 'rspec/support/ruby_features'
   block.call unless RSpec::Support::Ruby.jruby_9000?
 end
+
+Around "@broken-on-jruby-17" do |scenario, block|
+  require 'rspec/support/ruby_features'
+  block.call unless !RSpec::Support::Ruby.jruby? || RSpec::Support::Ruby.jruby_9000?
+end
