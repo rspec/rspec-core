9af661d13e06a57ec6427cf77a5e25899cff5864
Jon Rowe <hello@jonrowe.co.uk>
2022-02-09 09:59:26 +0000

    Version 3.11.0

--- lib/rspec/core/version.rb
+++ lib/rspec/core/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Core.
     module Version
       # Current version of RSpec Core, in semantic versioning format.
-      STRING = '3.11.0.pre'
+      STRING = '3.12.0.pre'
     end
   end
 end
