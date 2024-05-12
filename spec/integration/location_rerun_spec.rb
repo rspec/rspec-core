938470660e85401ee3ecc96a54f565dd7cb80324
Baden Ashford <baden.ashford@gmail.com>
2024-05-12 12:41:02 +1000

    Add some more specs

--- spec/integration/location_rerun_spec.rb
+++ spec/integration/location_rerun_spec.rb
@@ -1,52 +1,111 @@
 require 'support/aruba_support'
 
 RSpec.describe 'Failed spec rerun location' do
-  subject(:run_spec) do
-    file = cd('.') { "#{Dir.pwd}/failing_spec.rb" }
-    load file
-    run_command 'failing_spec.rb'
-  end
 
   include_context "aruba support"
 
   before do
     setup_aruba
-    write_file "failing_spec.rb", "
-            RSpec.describe do
-                shared_examples_for 'a failing spec' do
-                    it 'fails' do
-                        expect(1).to eq(2)
-                    end
-                end
+    write_file "some_examples.rb", "
+      RSpec.shared_examples_for 'a failing spec' do
+          it 'fails' do
+              expect(1).to eq(2)
+          end
+      end
+    "
 
-                context 'the first context' do
-                    it_behaves_like 'a failing spec'
-                end
+    file = cd('.') { "#{Dir.pwd}/some_examples.rb" }
+    load file
 
-                context 'the second context' do
-                    it_behaves_like 'a failing spec'
-                end
+    write_file "local_shared_examples_spec.rb", "
+      RSpec.describe do
+          shared_examples_for 'a failing spec' do
+              it 'fails' do
+                  expect(1).to eq(2)
+              end
+          end
+
+          context 'the first context' do
+              it_behaves_like 'a failing spec'
+          end
+
+          context 'the second context' do
+              it_behaves_like 'a failing spec'
+          end
+      end
+    "
+
+    write_file "non_local_shared_examples_spec.rb", "
+        RSpec.describe do
+            context 'the first context' do
+                it_behaves_like 'a failing spec'
             end
-        "
+
+            context 'the second context' do
+                it_behaves_like 'a failing spec'
+            end
+        end
+    "
   end
 
-  it 'prints the example ids' do
-    run_spec
+  context "when config.location_rerun_uses_line_number is set to false" do
+    it 'prints the example id of the failed assertion' do
+      run_command("#{Dir.pwd}/tmp/aruba/local_shared_examples_spec.rb")
 
-    expect(last_cmd_stdout).to include("/failing_spec.rb[1:1:1:1]")
-    expect(last_cmd_stdout).to include("/failing_spec.rb[1:2:1:1]")
+      expect(last_cmd_stdout).to include(<<-MSG
+Failed examples:
+
+rspec './local_shared_examples_spec.rb[1:1:1:1]' #  the first context behaves like a failing spec fails
+rspec './local_shared_examples_spec.rb[1:2:1:1]' #  the second context behaves like a failing spec fails
+      MSG
+      )
+    end
+    context "and the shared examples are defined in a separate file" do
+      it 'prints the line number where the `it_behaves_like` was called in the local file' do
+        run_command("#{Dir.pwd}/tmp/aruba/non_local_shared_examples_spec.rb")
+
+        expect(last_cmd_stdout).to include(<<-MSG
+Failed examples:
+
+rspec ./non_local_shared_examples_spec.rb:4 #  the first context behaves like a failing spec fails
+rspec ./non_local_shared_examples_spec.rb:8 #  the second context behaves like a failing spec fails
+        MSG
+        )
+      end
+    end
   end
 
   context "when config.location_rerun_uses_line_number is set to true" do
     before do
-      RSpec.configuration.location_rerun_uses_line_number = true
+      allow(RSpec.configuration).to receive(:location_rerun_uses_line_number).and_return(true)
     end
 
-    it 'prints the line numbers' do
-      run_spec
+    context "when the shared examples are defined in the same file as the spec" do
 
-      expect(last_cmd_stdout).to include("/failing_spec.rb:10")
-      expect(last_cmd_stdout).to include("/failing_spec.rb:14")
+      it 'prints the line number where the assertion failed in the local file' do
+        run_command("#{Dir.pwd}/tmp/aruba/local_shared_examples_spec.rb")
+
+        expect(last_cmd_stdout).to include(<<-MSG
+Failed examples:
+
+rspec ./local_shared_examples_spec.rb:4 #  the first context behaves like a failing spec fails
+rspec ./local_shared_examples_spec.rb:4 #  the second context behaves like a failing spec fails
+        MSG
+        )
+      end
+    end
+    context "and the shared examples are defined in a separate file" do
+      it 'prints the line number where the `it_behaves_like` was called in the local file' do
+
+        run_command("#{Dir.pwd}/tmp/aruba/non_local_shared_examples_spec.rb")
+        expect(last_cmd_stdout).to include(<<-MSG
+Failed examples:
+
+rspec ./non_local_shared_examples_spec.rb:4 #  the first context behaves like a failing spec fails
+rspec ./non_local_shared_examples_spec.rb:8 #  the second context behaves like a failing spec fails
+        MSG
+        )
+      end
     end
   end
 end
