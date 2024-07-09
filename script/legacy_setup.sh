e7ca6eb74df58c1ce95506c8ab8cb3e559544210
Jon Rowe <hello@jonrowe.co.uk>
2024-07-09 08:51:38 +0200

    Updated ci build scripts (from rspec-dev) main

--- script/legacy_setup.sh
+++ script/legacy_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This file was generated on 2024-02-23T14:21:31+00:00 from the rspec-dev repo.
+# This file was generated on 2024-07-09T08:51:38+02:00 from the rspec-dev repo.
 # DO NOT modify it by hand as your changes will get lost the next time it is generated.
 
 set -e
